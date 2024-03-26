0e73db1d51c5c55298bcc1646bbff64da0f4db93
Adi11cr7 <71431299+Adi11cr7@users.noreply.github.com>
2024-03-26 20:38:17 +0530

    Time: 592 ms (29.91%), Space: 58.7 MB (20.09%) - LeetHub

--- 0027-remove-element/0027-remove-element.scala
+++ 0027-remove-element/0027-remove-element.scala
@@ -2,15 +2,27 @@ import scala.util.Sorting
 object Solution {
     def removeElement(nums: Array[Int], `val`: Int): Int = {
         var k = 0
-        for(i<-0 until nums.length) {
-            if(nums(i) == `val`) {
-                nums(i) = 51
-                k += 1
+        var flag = false
+        var valCount = 0
+        var i = 0
+        while(i < nums.length && nums(i) != `val`) i+=1
+        // if(i<nums.length) valCount = 1
+        for(j <-i until nums.length) {
+            if(nums(j) != `val`) {
+                // k=i
+                nums(i) = nums(j)
+                nums(j) = 51
+                i += 1
+                
             }
+            else {
+                valCount +=1 
+            }
+            
         }
         // println(nums)
         // println(k)
-        Sorting.quickSort(nums)
-        nums.length-k
+        // Sorting.quickSort(nums)
+        nums.length-valCount
     }
 }
